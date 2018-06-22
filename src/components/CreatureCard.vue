a0efc200b98855d08ac63ecb9b5801b6b73e67de
Peter Andersen <peter@porksmash.com>
2018-06-21 22:58:38 -0700

    Added spells to creature card

--- src/components/CreatureCard.vue
+++ src/components/CreatureCard.vue
@@ -2,12 +2,18 @@
   <v-flex xs12 md6 lg3 fill-height>
     <v-card>
       <v-card-title>{{creature.name}} - {{creature.element}}</v-card-title>
+      <v-card-text>{{creature.rank}} Stars</v-card-text>
       <v-card-text>{{creature.lore}}</v-card-text>
+      <v-flex>
+        <Spell v-for="spell in creature.spells" :key="spell.id" :spell="spell" />
+      </v-flex>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+  import Spell from '@/components/Spell.vue';
+
   export default {
     name: "CreatureCard",
     props: {
@@ -15,6 +21,9 @@
         type: Object,
         required: true,
       }
+    },
+    components: {
+      Spell
     }
   }
 </script>
---
+++ src/components/Spell.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-avatar size="36px">
+    <img :src="imgUrl" :alt="spell.image" />
+  </v-avatar>
+</template>
+
+<script>
+  export default {
+    name: "Spell",
+    props: {
+      spell: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      imgUrl() {
+        return `/static/spells/${this.spell.image}.png`
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
