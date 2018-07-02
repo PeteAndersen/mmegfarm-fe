3dfa60dd02c9c9e01a00704c945185f0b2aa52db
Peter Andersen <peter@porksmash.com>
2018-07-02 12:53:29 -0700

    Show stats at max lvl instead of creature lore on card

--- src/components/CreatureCard.vue
+++ src/components/CreatureCard.vue
@@ -5,7 +5,58 @@
         <CreatureAvatar :creature="creature" />
         <h2>{{creature.name}}</h2>
       </v-card-title>
-      <v-card-text>{{creature.lore}}</v-card-text>
+      <v-container grid-list-md text-xs-center class="pa-2">
+        <v-layout row wrap>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-hp.png" />
+            </v-avatar>
+            {{creature.maxLvlHp}}
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-atk.png" />
+            </v-avatar>
+            {{creature.maxLvlAttack}}
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-def.png" />
+            </v-avatar>
+            {{creature.maxLvlDefense}}
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-speed.png" />
+            </v-avatar>
+            {{Math.round(creature.speed * 1000)}}
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-crit-rate.png" />
+            </v-avatar>
+            {{creature.criticalChance}}%
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-crit-dmg.png" />
+            </v-avatar>
+            {{creature.criticalDamage}}%
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-accuracy.png" />
+            </v-avatar>
+            {{Math.round(creature.accuracy * 100)}}%
+          </v-flex>
+          <v-flex xs3>
+            <v-avatar size="2em">
+              <img src="/static/creatures/icon-stats-resistance.png" />
+            </v-avatar>
+            {{Math.round(creature.resistance * 100)}}%
+          </v-flex>
+        </v-layout>
+      </v-container>
       <v-flex>
         <Spell v-for="spell in creature.spells" :key="spell.id" :spell="spell" />
       </v-flex>
