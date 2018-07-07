5aad90b5fccded65546fcf2568fa2cb20fc4973b
Peter <peter@porksmash.com>
2018-07-07 15:39:41 -0700

    Anonymize GA IP, removed new content snack because index.html is no longer cached

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@
     <v-footer app fixed>
       <p class="text-xs-center">&copy; 2018 Porksmash. Creature icons are &copy; Ubisoft</p>
     </v-footer>
-    <NewContentSnack />
   </v-app>
 </template>
 
