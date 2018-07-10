10f280ea61dc2701240f9a22e2cc326b51eeea5b
Peter <peter@porksmash.com>
2018-07-09 20:50:30 -0700

    Fleshing out CreatureDetail page. Need to break this into components

--- src/views/CreatureDetail.vue
+++ src/views/CreatureDetail.vue
@@ -1,18 +1,82 @@
 <template>
-  <v-container>
+  <v-container v-if="creature !== null" class="pt-0">
+    <v-toolbar>
+      <v-toolbar-items>
+        <v-btn to="/">
+          <v-icon>arrow_back</v-icon> Back
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-spacer />
+      
+      <v-toolbar-items>
+        <v-btn  v-for="fam in family" :key="fam.id" :to="`/creature/${fam.slug}`">
+          <v-avatar tile>
+            <img :src="`/static/creatures/icon-${fam.element}.png`" />
+          </v-avatar>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
     <v-layout>
-      <v-flex>
-        <CreatureAvatar :creature="creature" />
+      <v-flex xs12 md6>
+        <v-container>
+          <v-layout>
+            <h1>{{ creature.name }}</h1>
+
+            <v-spacer />
+
+            <v-tooltip bottom>
+              <v-avatar slot="activator">
+                <img :src="`/static/creatures/icon-${creature.archetype}.png`"/>
+              </v-avatar>
+              {{titleCase(creature.archetype)}}
+            </v-tooltip>
+            <v-tooltip bottom>
+              <v-avatar slot="activator">
+                <img :src="`/static/creatures/icon-${creature.element}.png`"/>
+              </v-avatar>
+              {{titleCase(creature.element)}}
+            </v-tooltip>
+          </v-layout>
+          <CreatureAvatar class="ma-3" :creature="creature" />
+        </v-container>
+      <v-layout>
+        <v-btn>
+          <creature-avatar :creature="creature" size="2em" :stars="false"/>
+          Evolves From
+        </v-btn>
+        <v-btn large>
+          Evolves To
+          <creature-avatar :creature="creature" size="2em" :stars="false"/>
+        </v-btn>
+      </v-layout>
       </v-flex>
-      {{slug}}
-      {{creature}}
+      <v-flex xs12 md6>
+        <v-container grid-list-lg>
+          <h2>Skills</h2>
+          <v-layout row>
+            <v-flex xs12 md6 v-for="(spell, index) in spellSlotOne" :key="index">
+              <SpellPanel :spell="spell" showSlot />
+            </v-flex>
+          </v-layout>
+          <v-layout column>
+            <v-flex v-for="(spell, index) in otherSpells" :key="index">
+              <SpellPanel :spell="spell" showSlot />
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import api from "@/api";
-import CreatureAvatar from '@/components/creatures/CreatureAvatar'
+import { mapActions, mapGetters } from "vuex";
+
+import { titleCase } from "@/services/utils";
+import CreatureAvatar from "@/components/creatures/CreatureAvatar";
+import SpellPanel from "@/components/creatures/SpellPanel";
 
 export default {
   name: "CreatureDetail",
@@ -23,25 +87,33 @@ export default {
     }
   },
   components: {
-    CreatureAvatar
+    CreatureAvatar,
+    SpellPanel
   },
-  data() {
-    return { creature: null };
+  watch: {
+    slug: function(val) {
+      // Update creature in state when slug changes
+      this.getCreature(val);
+    }
   },
-  created() {
+  created: async function() {
     this.getCreature(this.slug);
   },
   methods: {
-    getCreature: async function() {
-      const {
-        data: { results }
-      } = await api.get("/creatures/", {
-        params: {
-          slug: this.slug
-        }
-      });
-
-      this.creature = results[0];
+    ...mapActions(["getCreature"]),
+    titleCase
+  },
+  computed: {
+    ...mapGetters(["creature", "family"]),
+    spellSlotOne() {
+      if (this.creature) {
+        return this.creature.spells.filter(s => s.slot === 1);
+      }
+    },
+    otherSpells() {
+      if (this.creature) {
+        return this.creature.spells.filter(s => s.slot > 1);
+      }
     }
   }
 };
