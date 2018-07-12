99e3764cedc231977a87d1e110bf35d2ad9754a6
Peter Andersen <peter@porksmash.com>
2018-07-12 10:34:53 -0700

    Vue tracking package to get analytics on individual pages, not just initial load

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import VueAnalytics from "vue-analytics";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -8,6 +10,10 @@ import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
+Vue.use(VueAnalytics, {
+  id: "UA-63688026-2",
+  set: [{ field: "anonymizeIp", value: true }]
+});
 
 new Vue({
   router,
