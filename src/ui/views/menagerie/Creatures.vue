1529d3478723c3be4dfe7a00a0152e2b8a07a02a
Peter Andersen <peter@porksmash.com>
2018-07-20 07:46:17 -0700

    Fixed filter drawer button

--- src/ui/views/menagerie/Creatures.vue
+++ src/ui/views/menagerie/Creatures.vue
@@ -6,7 +6,7 @@
       <v-layout row align-baseline class="pb-2">
         
         <v-flex>
-          <v-btn @click="$store.state.filterDrawer = !$store.state.filterDrawer" class="ml-0">
+          <v-btn @click="$store.state.menagerie.filterDrawer = !$store.state.menagerie.filterDrawer" class="ml-0">
             <v-icon class="pr-1">menu</v-icon> Filters
           </v-btn>
 
